f898a6f324e4031fcd6f07c8f2e2ac24594af45e
jasarmig <N0seasap@ksozr>
2022-11-05 22:13:37 -0500

    added priority to tests to execute in order

--- src/test/java/org/testnganotations/tests/Tests.java
+++ src/test/java/org/testnganotations/tests/Tests.java
@@ -5,17 +5,17 @@ import org.testng.annotations.*;
 
 public class Tests {
 
-    @Test
+    @Test (priority = 0)
     public void loginTest(){
         System.out.println("Login in the app");
     }
 
-    @Test
+    @Test (priority = 1)
     public void deactivateTest(){
         System.out.println("User deactivated successfully!");
     }
 
-    @Test
+    @Test (priority = 2)
     public void logoutTest(){
         System.out.println("Login out the app");
     }
