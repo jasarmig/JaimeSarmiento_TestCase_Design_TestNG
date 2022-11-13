cc09ad09a0c8d0943ac58d89d5ab5c3f6a24d882
jasarmig <N0seasap@ksozr>
2022-11-12 23:01:49 -0500

    Refactored resource bundle

--- src/test/java/org/espn/pages/BasePage.java
+++ src/test/java/org/espn/pages/BasePage.java
@@ -1,10 +1,18 @@
 package org.espn.pages;
 import org.espn.config.WebOps;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class BasePage extends WebOps {
 
     public BasePage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(id = "global-scoreboard")
+    private WebElement topScoreboard;
+
+    //TODO declare all common elements using @FindBy
 }
